7da5ffd3e0c51cb58ea635905ebf6ecafe401be1
Patrick Clary <pclary@outlook.com>
2016-05-16 12:25:27 -0700

    Teleop works again (video temporarily commented out)

--- teleop.py
+++ teleop.py
@@ -3,10 +3,10 @@ import sys
 import time
 import serial
 import math
-import cv2
+# import cv2
 
-camera = cv2.VideoCapture(0)
-camera.set(cv2.cv.CV_CAP_PROP_FPS, 60)
+# camera = cv2.VideoCapture(0)
+# camera.set(cv2.cv.CV_CAP_PROP_FPS, 60)
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
@@ -22,11 +22,11 @@ def clamp(n, minn, maxn):
 
 
 def command(x, dx, pan, dpan, tilt, dtilt, home):
-    x = clamp(int(pan*65535), 0, 65534);
+    x = clamp(int(x*65535), 0, 65534);
     xh = x >> 8 & 0xff
     xl = x & 0xff
 
-    dx = clamp(int(x*255), 0, 254)
+    dx = clamp(int(dx*255), 0, 254)
 
     pan = clamp(int(pan*32767), -32767, 32767);
     panh = pan >> 8 & 0xff
@@ -49,7 +49,7 @@ def command(x, dx, pan, dpan, tilt, dtilt, home):
 
     flags = 0x00
     if home:
-        flags = flags & 0x01
+        flags = flags | 0x01
 
     checksum = (xh + xl + dx + panh + panl + tilth + tiltl + dtilt + dpan + flags) & 0x7f
     ser.write(bytearray([0xff, 0xff, dx, xh, xl, tilth, tiltl, panh, panl, dtilt, dpan, flags, checksum]))
@@ -66,8 +66,9 @@ dt = 1/60.0
 panlast = 0
 tiltlast = 0
 xlast = 0
+dx = 0
 
-command(0, 0, 0, 0, 0, 0, True)
+command(0, 0, pan, 0, tilt, 0, True)
 
 while True:
     keyState = pygame.key.get_pressed()
@@ -77,11 +78,11 @@ while True:
 
     factor = 1
     if keyState[pygame.K_LSHIFT]:
-        factor = 0.03
+        factor = 0.3
 
-    if keyState[pygame.K_LEFT]:
+    if keyState[pygame.K_LEFT] and x < 1:
         x += 0.01*factor
-    if keyState[pygame.K_RIGHT]:
+    if keyState[pygame.K_RIGHT] and x > 0:
         x -= 0.01*factor
     if keyState[pygame.K_UP] and pan < 1:
         pan += 0.005*factor
@@ -103,16 +104,19 @@ while True:
 
     dtilt = (tilt - tiltlast)/dt
     dpan = (pan - panlast)/dt
-    dx = 0.2
+
+    xdiff = abs(x - xlast)
+    if xdiff > 0:
+        dx = xdiff/dt * 0.45
 
     command(x, dx, pan, dpan, tilt, dtilt, False)
 
-    ret, frame = camera.read()
-    cv2.imshow('fish', frame)
-    cv2.waitKey(1)
-    # time.sleep(dt)
+    # ret, frame = camera.read()
+    # cv2.imshow('fish', frame)
+    # cv2.waitKey(1)
+    time.sleep(dt)
 
     pygame.event.pump()
 
-camera.release()
-cv2.destroyAllWindows()
+# camera.release()
+# cv2.destroyAllWindows()
